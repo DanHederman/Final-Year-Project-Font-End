0edff2a6d8d5c185dea5826ff05224846f84207b
Dan Hederman <c15410232@mydit.ie>
2018-12-20 10:27:15 +0000

    Update

--- Work/app/src/main/java/com/abc/work/MainActivity.java
+++ Work/app/src/main/java/com/abc/work/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     private Handler handler;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
